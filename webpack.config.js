b926af1f6fb465c8464df4f4af1164eb498f9eed
thir13en <santilpzate@gmail.com>
2020-07-13 19:27:46 +0200

    updated node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin';
 
 const projectName = 'JSCamp 2019 slides';
 const templatePath = 'src/index.template.html';
